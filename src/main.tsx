253ac02fe0b394fa693f9c820419e48b2a9c4a9a
iksflow <theiksflow@gmail.com>
2024-03-24 18:24:07 +0900

    Remove wrong code

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 
 import "./tailwind.css";
-import SampleProvider from "./SampleProvider.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <SampleProvider>
-      <App />
-    </SampleProvider>
+    <App />
   </React.StrictMode>
 );
