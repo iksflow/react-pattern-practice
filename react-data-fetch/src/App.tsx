9cd12a56fccd8532b3909e94c1fb4a8aea9e21fa
iksflow <theiksflow@gmail.com>
2025-01-15 17:51:13 +0900

    init data-fecth project

---
+++ react-data-fetch/src/App.tsx
@@ -0,0 +1,12 @@
+import User from '@/components/User';
+function App() {
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center h-screen mx-auto">
+        <User />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ react-data-fetch/src/components/User.tsx
@@ -0,0 +1,5 @@
+const User = () => {
+  return <div className="text-2xl font-bold">User Component</div>;
+};
+
+export default User;
