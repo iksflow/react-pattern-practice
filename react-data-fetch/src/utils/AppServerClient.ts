7095739e1b3cc1dfcaddecd33dc3dfd48ed25ed6
iksflow <theiksflow@gmail.com>
2025-01-20 11:10:11 +0900

    Resolve conflict

--- react-data-fetch/src/utils/AppServerClient.ts
+++ react-data-fetch/src/utils/AppServerClient.ts
@@ -1,16 +1,12 @@
 export class AppServerClient {
-  private static instance: AppServerClient;
-  private constructor() {}
+  private baseURL = 'https://jsonplaceholder.typicode.com';
 
-  public static getInstance() {
-    if (!AppServerClient.instance) {
-      AppServerClient.instance = new AppServerClient();
-    }
-    return AppServerClient.instance;
-  }
-
-  public async getTodos() {
-    const response = await fetch('https://jsonplaceholder.typicode.com/todos');
-    return response.json();
+  async get(endpoint: string): Promise<any> {
+    return fetch(`${this.baseURL}${endpoint}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
   }
 }
