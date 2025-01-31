5ca9837d61f9896d32e805c14b069ae1ac0cb886
iksflow <theiksflow@gmail.com>
2025-01-31 12:20:21 +0900

    Add sliced reducers to the rootReducer.

--- redux-fundamentals-example-app/src/reducer.js
+++ redux-fundamentals-example-app/src/reducer.js
@@ -1,22 +1,10 @@
-const initialState = {
-  todos: [
-    { id: 0, text: 'Learn React', completed: true },
-    { id: 1, text: 'Learn Redux', completed: false, color: 'purple' },
-    { id: 2, text: 'Build something fun!', completed: false, color: 'blue' },
-  ],
-  filters: {
-    status: 'All',
-    colors: [],
-  },
-};
+import todosReducer from './features/todos/todoSlice';
+import filtersReducer from './features/filters/filterSlice';
 
 // Use the initialState as a default value
-export default function appReducer(state = initialState, action) {
-  // The reducer normally looks at the action type field to decide what happens
-  switch (action.type) {
-    default:
-      // If this reducer doesn't recognize the action type, or doesn't
-      // care about this specific action, return the existing state unchanged
-      return state;
-  }
+export default function rootReducer(state = {}, action) {
+  return {
+    todos: todosReducer(state.todos, action),
+    filters: filtersReducer(state.filters, action),
+  };
 }
