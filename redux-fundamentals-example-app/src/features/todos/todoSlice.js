1da7ead2503bc0271bd10637835272907bb9ef27
iksflow <theiksflow@gmail.com>
2025-01-31 12:11:24 +0900

    Split the root reducer into slices

---
+++ redux-fundamentals-example-app/src/features/todos/todoSlice.js
@@ -0,0 +1,41 @@
+const initialState = [
+  { id: 0, text: 'Learn React', completed: true },
+  { id: 1, text: 'Learn Redux', completed: false, color: 'purple' },
+  { id: 2, text: 'Build something fun!', completed: false, color: 'blue' },
+];
+
+function nextTodoId(todos) {
+  const maxId = todos.reduce((maxId, todo) => Math.max(todo.id, maxId), -1);
+  return maxId + 1;
+}
+
+export default function todosReducer(state = initialState, action) {
+  switch (action.type) {
+    case 'todos/todoAdded': {
+      return [
+        ...state,
+        {
+          id: nextTodoId(state.todos),
+          text: action.payload,
+          completed: false,
+        },
+      ];
+    }
+    case 'todos/todoToggled': {
+      return state.map((todo) => {
+        // If this isn't the todo item we're looking for, leave it alone
+        if (todo.id !== action.payload) {
+          return todo;
+        }
+
+        // We've found the todo that has to change. Return a copy:
+        return {
+          ...todo,
+          completed: !todo.completed,
+        };
+      });
+    }
+    default:
+      return state;
+  }
+}
