d01a2d6fd6b4d7b2fd2cacbba2272fdd20ff08ed
iksflow <theiksflow@gmail.com>
2025-01-31 12:26:39 +0900

    Use combinedReducers from the Redux core library

--- redux-fundamentals-example-app/src/reducer.js
+++ redux-fundamentals-example-app/src/reducer.js
@@ -1,10 +1,10 @@
 import todosReducer from './features/todos/todoSlice';
 import filtersReducer from './features/filters/filterSlice';
+import { combineReducers } from 'redux';
 
-// Use the initialState as a default value
-export default function rootReducer(state = {}, action) {
-  return {
-    todos: todosReducer(state.todos, action),
-    filters: filtersReducer(state.filters, action),
-  };
-}
+const rootReducer = combineReducers({
+  todos: todosReducer,
+  filters: filtersReducer,
+});
+
+export default rootReducer;
